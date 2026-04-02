c8ece127fda8c520af7e8086901feedaf811e494
Nurulloxon <kenjaxojayevnurulloxon@gmail.com>
2026-04-02 10:20:48 +0500

    Google ulanish usuli Railway uchun moslashtirildi

--- config.py
+++ config.py
@@ -12,23 +12,9 @@ logger.info("[CONFIG] .env faylidan muhit o'zgaruvchilari yuklanmoqda...")
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-GOOGLE_CREDENTIALS_JSON_PATH = os.getenv("GOOGLE_CREDENTIALS_JSON_PATH")
+# DIQQAT: Bu yerdan _PATH so'zi olib tashlandi
+GOOGLE_CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON")
 SHEET_NAME = "Elshift"
-
-try:
-    LOGIST_GROUP_ID = int(os.getenv("LOGIST_GROUP_ID", 0))
-    ABOUT_US_MSG_ID = int(os.getenv("ABOUT_US_MSG_ID", 0))
-    CONTACT_MSG_ID = int(os.getenv("CONTACT_MSG_ID", 0))
-    NEW_CLIENT_INFO_MSG_ID = int(os.getenv("NEW_CLIENT_INFO_MSG_ID", 0))
-except ValueError:
-    LOGIST_GROUP_ID = 0
-    ABOUT_US_MSG_ID = 0
-    CONTACT_MSG_ID = 0
-    NEW_CLIENT_INFO_MSG_ID = 0
-
-if not all([BOT_TOKEN, GOOGLE_CREDENTIALS_JSON_PATH]) or LOGIST_GROUP_ID == 0:
-    logger.critical("[CONFIG] DIQQAT: .env faylida ma'lumotlar yetishmayapti yoki xato!")
-    exit(1)
 
 try:
     LOGIST_GROUP_ID = int(os.getenv("LOGIST_GROUP_ID", 0))
@@ -40,6 +26,7 @@ except ValueError:
 
 WAKE_SCHEDULE_TIMES = os.getenv("WAKE_SCHEDULE_TIMES", "09:00,14:00,16:00").split(",")
 
-if not all([BOT_TOKEN, GOOGLE_CREDENTIALS_JSON_PATH]) or LOGIST_GROUP_ID == 0:
-    logger.critical("[CONFIG] DIQQAT: .env faylida ma'lumotlar yetishmayapti yoki xato!")
+# DIQQAT: Bu yerda ham _PATH so'zi olib tashlandi
+if not all([BOT_TOKEN, GOOGLE_CREDENTIALS_JSON]) or LOGIST_GROUP_ID == 0:
+    logger.critical("[CONFIG] DIQQAT: muhit o'zgaruvchilari (Variables) yetishmayapti yoki xato!")
     exit(1)
